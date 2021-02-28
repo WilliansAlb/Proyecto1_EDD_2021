ee9ab6ca8e9d5f44ec3f7d033cf6334c1914ca2a
WilliansAlb <williansalbertolpez@hotmail.com>
2021-02-28 00:53:41 -0600

    Creación de pilas de carretas, ingreso de clientes a la cola de espera y comienzo del metodo de agarrar carreta

--- Proyecto1.c
+++ Proyecto1.c
@@ -7,22 +7,56 @@
 typedef struct Cliente
 {
     int identificador;
+    int carreta;
     struct Cliente *siguiente;
 } Cliente;
+
+typedef struct Caja
+{
+    int no_caja;
+    int id_cliente;
+    int tiempo_servicio;
+    char estado[10];
+    int carreta_cliente;
+    struct Caja *siguiente;
+} Caja;
+
+typedef struct Carreta
+{
+    int no_carreta;
+    struct Carreta *siguiente;
+} Carreta;
 
 typedef struct ListaClientes
 {
     Cliente *primero;
     Cliente *ultimo;
-    int longitud;
     int conteo;
 } ListaClientes;
 
-ListaClientes lista;
-
-void ingresarCliente(int numero)
+typedef struct Cola
 {
-    Cliente *nuevo = (Cliente *)malloc(sizeof(Cliente));
+    Cliente *primero;
+    int conteo;
+} Cola;
+
+typedef struct Pila
+{
+    Carreta *primera;
+    Carreta *ultima;
+    int longitud;
+} Pila;
+
+ListaClientes lista;
+Cola cola_espera;
+Cola cola_pago;
+Pila carreta_1;
+Pila carreta_2;
+
+//Métodos de ingreso, obtención y eliminación de clientes que entran al bucle de compras
+void ingresarACicloCompras(Cliente *aIngresar)
+{
+    Cliente *nuevo = aIngresar;
     lista.conteo++;
     nuevo->identificador = lista.conteo;
     if (lista.primero == NULL)
@@ -30,7 +64,6 @@ void ingresarCliente(int numero)
         lista.primero = nuevo;
         lista.ultimo = nuevo;
         nuevo->siguiente = nuevo;
-        lista.longitud++;
     }
     else
     {
@@ -39,20 +72,18 @@ void ingresarCliente(int numero)
         nuevo->siguiente = lista.primero;
         aux->siguiente = nuevo;
         lista.ultimo = nuevo;
-        lista.longitud++;
     }
 }
-
 void imprimirClientes()
 {
     Cliente *temp = lista.primero;
     do
     {
-        printf("%i\n", temp->identificador);
+        printf("%i - ", temp->identificador);
         temp = temp->siguiente;
     } while (temp != lista.primero);
+    printf("\n");
 }
-
 int saleDeCompras()
 {
     Cliente *temp = lista.primero;
@@ -60,7 +91,7 @@ int saleDeCompras()
     while (id == -1)
     {
         srand(time(NULL));
-        num = rand() % (100 + 1);
+        num = rand() % (10 - 1) + 1;
         do
         {
             if (num == temp->identificador)
@@ -76,26 +107,188 @@ int saleDeCompras()
     }
     return id;
 }
+void eliminarClienteDeCompras(int numero)
+{
+    Cliente *temp = lista.primero;
+    Cliente *aux;
+    if (temp->identificador == numero)
+    {
+        aux = temp;
+        temp = temp->siguiente;
+        lista.ultimo->siguiente = temp;
+        lista.primero = temp;
+        free(aux);
+    }
+    else
+    {
+        do
+        {
+            if (temp->siguiente->identificador == numero)
+            {
+                aux = temp->siguiente;
+                temp->siguiente = temp->siguiente->siguiente;
+                free(aux);
+                break;
+            }
+            else
+            {
+                temp = temp->siguiente;
+            }
+        } while (temp != lista.primero);
+    }
+}
 
+//Métodos de ingreso, obtención y eliminación de colas
+void ingresarAColaEspera()
+{
+    Cliente *nuevo = (Cliente *)malloc(sizeof(Cliente));
+    nuevo->siguiente = NULL;
+    Cliente *aux;
+    if (cola_espera.primero == NULL)
+    {
+        cola_espera.conteo = 1;
+        nuevo->identificador = 1;
+        cola_espera.conteo++;
+        cola_espera.primero = nuevo;
+    }
+    else
+    {
+        aux = cola_espera.primero;
+        while (aux->siguiente != NULL)
+        {
+            aux = aux->siguiente;
+        }
+        nuevo->identificador = cola_espera.conteo;
+        cola_espera.conteo++;
+        aux->siguiente = nuevo;
+    }
+}
+Cliente *eliminarDeColaEspera()
+{
+    Cliente *aux;
+    if (cola_espera.primero != NULL)
+    {
+        if (cola_espera.primero->siguiente == NULL)
+        {
+            aux = cola_espera.primero;
+            cola_espera.primero = NULL;
+            return aux;
+        }
+        else
+        {
+            aux = cola_espera.primero;
+            cola_espera.primero = cola_espera.primero->siguiente;
+            return aux;
+        }
+    }
+    else
+    {
+        return NULL;
+    }
+}
+void imprimirCola(Cola imp)
+{
+    Cliente *temp = imp.primero;
+    do
+    {
+        printf("%i - ", temp->identificador);
+        temp = temp->siguiente;
+    } while (temp != NULL);
+    printf("\n");
+}
+
+//Métodos de ingreso y obtención de las carretas
+void ingresarCarreta1(int noCarreta, int pila_no)
+{
+    Carreta *nueva = (Carreta *)malloc(sizeof(Carreta));
+    Carreta *aux;
+    nueva->no_carreta = noCarreta;
+    nueva->siguiente = NULL;
+    if (pila_no == 0)
+    {
+        if (carreta_1.primera == NULL)
+        {
+            carreta_1.primera = nueva;
+            carreta_1.ultima = nueva;
+            carreta_1.longitud++;
+        }
+        else
+        {
+            aux = carreta_1.primera;
+            while (aux->siguiente != NULL)
+            {
+                aux = aux->siguiente;
+            }
+            aux->siguiente = nueva;
+            carreta_1.ultima = nueva;
+            carreta_1.longitud++;
+        }
+    } else {
+        if (carreta_2.primera == NULL)
+        {
+            carreta_2.primera = nueva;
+            carreta_2.ultima = nueva;
+            carreta_2.longitud++;
+        }
+        else
+        {
+            aux = carreta_2.primera;
+            while (aux->siguiente != NULL)
+            {
+                aux = aux->siguiente;
+            }
+            aux->siguiente = nueva;
+            carreta_2.ultima = nueva;
+            carreta_2.longitud++;
+        }
+    }
+}
+void crearPilas()
+{
+    srand(time(NULL));
+    for (int s = 0; s < NO_CARRETAS; s++)
+    {
+        int num = rand() % 2;
+        if (num == 0)
+        {
+            ingresarCarreta1(s + 1,num);
+        }
+        else
+        {
+            ingresarCarreta1(s + 1,num);
+        }
+    }
+}
+void imprimirCarreta(Pila pil)
+{
+    Carreta *temp = pil.primera;
+    do
+    {
+        printf("%i - ", temp->no_carreta);
+        temp = temp->siguiente;
+    } while (temp != NULL);
+    printf("\n");
+}
 int main()
 {
+    crearPilas();
     int cuantos;
-    printf("Ingresa cuantos clientes entran al minimarket\n");
-    scanf("%i", &cuantos);
-    for (int i = 0; i < cuantos; i++)
-    {
-        ingresarCliente(i + 1);
+    printf("Cuantos clientes nuevos ingresan?\n");
+    scanf("%i",&cuantos);
+    for (int a = 0; a < cuantos; a++){
+        ingresarAColaEspera();
     }
-    imprimirClientes();
-    
-    printf("Ingresa cuantos clientes entran al minimarket\n");
-    scanf("%i", &cuantos);
-    for (int i = 0; i < cuantos; i++)
-    {
-        ingresarCliente(i + 1);
-    }
-    imprimirClientes();
-    //printf("El cliente que salio de comprar es: %i", saleDeCompras());
-    system("dot -Tpng grafoPrueba.txt -o grafoPrueba.png");
+    printf("Pila 1 de carretas: ");
+    imprimirCarreta(carreta_1);
+    printf("Pila 2 de carretas: ");
+    imprimirCarreta(carreta_2);
+    printf("Clientes en espera: ");
+    imprimirCola(cola_espera);
+    //imprimirClientes();
+    //int sale = saleDeCompras();
+    //printf("El cliente que salio de comprar es: %i\n", sale);
+    //eliminarClienteDeCompras(sale);
+    //imprimirClientes();
+    //system("dot -Tpng grafoPrueba.txt -o grafoPrueba.png");
     return 0;
 }
